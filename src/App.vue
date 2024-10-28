74aa6cadc1be6272e526da6f19938b1e529a22d1
Haixing Hu <starfish.hu@gmail.com>
2024-10-28 12:18:25 +0800

    refactor: refactor

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ class App {
     this.logger.info('正在启动：%s v%s', config.app_name, config.app_version);
     this.logger.info('Debugging Model:', config.debug);
     this.logger.info('Node ENV:', config.node_env);
-    if (config.get('disable_loading', false)) {
+    if (config.disable_loading) {
       // disable loading
       this.logger.info('Disable the loading');
       loading.disable();
